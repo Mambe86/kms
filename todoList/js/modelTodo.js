fd2ccb917a434892444bc5112b7b513bf58b677b
Mambe86 <58339882+Mambe86@users.noreply.github.com>
2020-03-23 23:05:37 +0300

    storage class was added

--- todoList/js/modelTodo.js
+++ todoList/js/modelTodo.js
@@ -4,4 +4,66 @@ class Todo{
         this.title = title;
         this.check = check;
     }
+    
+
+}
+
+class StorageTodo {
+    getLastId () {
+        throw new Error("Нужно реализовать");
+    }
+    generateNewId () {
+        throw new Error("Нужно реализовать");
+    }
+    getListTodo () {
+        throw new Error("Нужно реализовать");
+    }
+    getTodoById (id) {
+        throw new Error("Нужно реализовать");
+    }
+    deleteTodoById(id) {
+        throw new Error("Нужно реализовать");
+    }
+    changeCheckTodoById (id, check){
+        throw new Error("Нужно реализовать");
+    }
+    saveTodo (todo) {
+        throw new Error("Нужно реализовать");
+    }
+}
+class LocalStorageTodo extends StorageTodo {
+    getLastId () {
+        let lastId = JSON.parse(localStorage.getItem("todoLastId"));
+        if (lastId == null) lastId=0;
+        return parseInt(lastId,10);
+    }
+    generateNewId () {
+        let newId = this.getLastId() +1;
+        localStorage.setItem("todoLastId", JSON.stringify(newId));
+        return newId;
+    }
+    getListTodo () {
+        let listTodo = JSON.parse(localStorage.getItem("listTodo"));
+        if (listTodo == null) listTodo = {};
+        return listTodo;
+    }
+    getTodoById (id) {
+       return this.getListTodo()[id];
+    }
+    deleteTodoById(id) {
+        let listTodo = this.getListTodo();
+        listTodo.splice(id,1);
+        localStorage.setItem("listTodo", JSON.stringify(listTodo));
+
+    }
+    changeCheckTodoById (id, check){
+        let listTodo = this.getListTodo();
+        listTodo[id].check = check;
+        localStorage.setItem("listTodo", JSON.stringify(listTodo));
+    }
+    addTodo (todo) {
+        let listTodo = this.getListTodo();
+        listTodo[todo.id] = todo;
+        localStorage.setItem("listTodo", JSON.stringify(listTodo));
+    }
 }
--- todoList/js/script.js
+++ todoList/js/script.js
@@ -1,50 +1,54 @@
 window.onload = function() {
-    todoList = [];
-    if (localStorage.getItem("todo") != undefined) {
-        todoList = JSON.parse(localStorage.getItem("todo"));
-        out();
-    }
+    let storageTodo = new LocalStorageTodo();
+    let todoList = storageTodo.getListTodo();
+    out();
+
     document.getElementById("add").onclick = function() {
         let title = document.getElementById("in").value;
-       let id =todoList.length;
-              
-        todoList[id] = new Todo(id, title, false);
-        
-        console.log(todoList);
+        let todo = new Todo(storageTodo.generateNewId(), title, false);
+        storageTodo.addTodo(todo);
+
+        console.log(storageTodo.getListTodo());
         out();
-        localStorage.setItem("todo", JSON.stringify(todoList));
     };
 
-///При добавлении нового туду зачеркивание пропадает на старых туду
-
+    ///При добавлении нового туду зачеркивание пропадает на старых туду
 
     function out() {
         let out = "";
+        let todoList = storageTodo.getListTodo();
         for (let key in todoList) {
             out += `<label>`;
-           let checked = "";
+            let checked = "";
             if (todoList[key].check == true) {
                 checked = "checked";
             }
-            out += `<input type="checkbox" id ="${todoList[key].id}" ${checked}>`;            
+            out += `<input type="checkbox" id ="${todoList[key].id}" ${checked}>`;
             out += todoList[key].title;
             out += `</label>`;
             out += `<br>`;
-       
         }
 
         document.getElementById("out").innerHTML = out;
     }
 
     document.getElementById("out").onclick = function(e) {
+        let todoList = storageTodo.getListTodo();
         console.log(e.target.id);
-         if ((document.getElementById(e.target.id).type == "checkbox")&&(document.getElementById(e.target.id).checked)){
-            todoList[e.target.id].check=true; 
-            document.getElementById(e.target.id).parentElement.style.textDecoration="line-through";
-        }
-        else if (document.getElementById(e.target.id).type == "checkbox") {
-            todoList[e.target.id].check=false;
-            document.getElementById(e.target.id).parentElement.style.textDecoration="none";
+
+        if (document.getElementById(e.target.id).type == "checkbox") {
+            let isCheck = document.getElementById(e.target.id).checked;
+
+            if (isCheck) {
+                document.getElementById(
+                    e.target.id
+                ).parentElement.style.textDecoration = "line-through";
+            } else {
+                document.getElementById(
+                    e.target.id
+                ).parentElement.style.textDecoration = "none";
+            }
+            storageTodo.changeCheckTodoById(e.target.id, isCheck);
         }
     };
 };
